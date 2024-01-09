3e6c12401263556b718b2b4dc275cfcf6fc80ba1
Zain-Khoso <zain.khoso.dev@gmail.com>
2024-01-09 09:53:15 +0500

    initial-commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./style.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <h1>Hello World</h1>
+    </React.StrictMode>
+);
