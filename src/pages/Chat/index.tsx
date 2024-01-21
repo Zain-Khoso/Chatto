16821d429d7b885b2885a2ef7ee94e5fc7163f02
Zain-Khoso <zain.khoso.dev@gmail.com>
2024-01-21 13:07:48 +0500

    UI Fixes

--- src/pages/Chat/index.tsx
+++ src/pages/Chat/index.tsx
@@ -1,8 +1,17 @@
+// Utils
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "@/configs/firebase";
+import useNavigateHome from "@/hooks/useNavigateHome";
+
 // Components
 import SideNav from "./components/SideNav";
 import Messages from "./components/Messages";
 
 export default function Chat() {
+    const [user, _, error] = useAuthState(auth);
+
+    useNavigateHome(!user || error ? true : false);
+
     return (
         <>
             <SideNav />
